e9c1dc67eda1dbebd3bb513713b9a83d02240add
eiglerl <eiglerlukas@gmail.com>
2023-10-12 16:51:57 +0200

    dominated strategies

--- libs/week1.py
+++ libs/week1.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-
-import numpy as np
-
-
 def compute_non_zero_sum_game_value(matrix1, matrix2, p1_strat, p2_strat):
     # create probability matrix
     prob_matrix = p2_strat @ p1_strat
@@ -41,3 +37,55 @@ def best_response_value_column(matrix, column_strategy):
     response = best_response_column(matrix, column_strategy)
     p1_val, p2_val = compute_non_zero_sum_game_value(-matrix, matrix, response, column_strategy)
     return p2_val
+
+def find_dominated_actions(matrix, axis):
+    dominated_actions = []
+    # print("axis:", axis)
+    for i in range(matrix.shape[axis]):
+        for j in range(matrix.shape[axis]):
+            if i >= j:
+                continue
+            # print(np.take(matrix, i, axis=axis), "vs",np.take(matrix, j, axis=axis))
+            if np.all(np.take(matrix, i, axis=axis) >= np.take(matrix, j, axis=axis)):
+                dominated_actions.append(j)
+    return dominated_actions
+
+
+def find_dominated(matrix1, matrix2):
+    dominated_rows = find_dominated_actions(matrix1, axis=0)
+    dominated_columns = find_dominated_actions(matrix2, axis=1)
+    return dominated_rows, dominated_columns
+
+def iterated_removal_of_dominated_strategies(matrix1, matrix2):
+    temp1 = matrix1[:]
+    temp2 = matrix2[:]
+    while True:
+        dominated_rows, dominated_columns = find_dominated(temp1, temp2)
+        if len(dominated_rows) + len(dominated_columns) == 0:
+            break
+    
+        non_dominated_mask = np.ones(temp1.shape[0], dtype=bool)
+        non_dominated_mask[dominated_rows] = False
+        
+        temp1 = temp1[non_dominated_mask]
+        temp2 = temp2[non_dominated_mask]
+
+        non_dominated_mask = np.ones(temp1.shape[1], dtype=bool)
+        non_dominated_mask[dominated_columns] = False
+
+        temp1 = temp1[:,non_dominated_mask]
+        temp2 = temp2[:,non_dominated_mask]
+        
+        
+    return temp1, temp2
+
+        
+
+matrix1 = np.array([[13,1,7], [4,3,6], [-1,2,8]])
+matrix2 = np.array([[3,4,3], [1,3,2], [9,8,-1]])
+print(matrix1)
+print(matrix2, "\n")
+temp1, temp2 = iterated_removal_of_dominated_strategies(matrix1, matrix2)
+print(temp1)
+print(temp2)
+        

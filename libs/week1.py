fixed delta computation

libs/week1.py
@@ -12,13 +12,20 @@ def compute_non_zero_sum_game_value(matrix1, matrix2, p1_strat, p2_strat):
 
 def best_response(matrix, strat, for_row=True):
     expected_payoffs = (strat @ matrix) if for_row else (matrix @ strat)
-    len = expected_payoffs.shape[1] if for_row else expected_payoffs.shape[0]
-    max_payoff = np.argmax(expected_payoffs)
-    best_response = create_pure_strategy(len, max_payoff)
+    axis = 1 if for_row else 0
+    # len = expected_payoffs.shape[1] if for_row else expected_payoffs.shape[0]
+    len = expected_payoffs.shape[axis]
+    # print(f"excepted pay off ","row " if for_row else "col ", expected_payoffs)
+    max_payoff = np.argmax(expected_payoffs, axis=axis)
+    # print(f"max payoff {max_payoff} -> {np.take(expected_payoffs, max_payoff, axis=axis)}")
+    best_response = create_pure_strategy(len, max_payoff, transpose=for_row)
     return best_response
 
-def create_pure_strategy(len, index):
-    return np.array([[1 if i==index else 0 for i in range(len)]])
+def create_pure_strategy(len, index, transpose):
+    response = np.array([[1 if i==index else 0 for i in range(len)]])
+    if transpose:
+        return response.T
+    return response
 
 def find_dominated_actions(matrix, axis):
     dominated_actions = []
@@ -79,14 +86,14 @@ def evaluate(matrix: np.array, row_strategy: np.array, column_strategy: np.array
 
 
 def best_response_value_row(matrix: np.array, row_strategy: np.array) -> float:
-    """Value of the row player when facing a best-responding column player"""
+    """Value of the row player when facing a best-responding column player in a zero-sum game"""
     response = best_response(-matrix, row_strategy, for_row=True)
-    p1_val, p2_val = compute_non_zero_sum_game_value(-matrix, matrix, row_strategy, response.T)
+    p1_val, p2_val = compute_non_zero_sum_game_value(-matrix, matrix, row_strategy, response)
     return p1_val
 
 
 def best_response_value_column(matrix: np.array, column_strategy: np.array) -> float:
-    """Value of the column player when facing a best-responding row player"""
+    """Value of the column player when facing a best-responding row player in a zero-sum game"""
     response = best_response(matrix, column_strategy, for_row=False)
     p1_val, p2_val = compute_non_zero_sum_game_value(-matrix, matrix, response, column_strategy)
     return p2_val

libs/week3.py
@@ -3,16 +3,15 @@ import week1 as week1
 
 
 def compute_deltas(matrix: np.array, row_strategy: np.array, column_strategy: np.array) -> np.array:
-    """Computer how much the players could improve if they were to switch to a best response"""    
-    
-    best_row_strategy = week1.best_response(matrix, column_strategy, for_row=False)
-    best_col_strategy = week1.best_response(-matrix, row_strategy, for_row=True)
+    """Compute how much the players could improve if they were to switch to a best response"""    
+    # current utility for both row and column player
+    utility_row, utility_col = week1.compute_non_zero_sum_game_value(-matrix, matrix, row_strategy, column_strategy)
 
-    utility_row, utility_col = week1.compute_non_zero_sum_game_value(matrix, -matrix, row_strategy, column_strategy)
-    best_row_utility, _ = week1.compute_non_zero_sum_game_value(matrix, -matrix, best_row_strategy, column_strategy)
-    _, best_col_utility = week1.compute_non_zero_sum_game_value(matrix, -matrix, row_strategy, best_col_strategy.T)
-    print(f"col {best_col_utility}-{utility_col}={best_col_utility-utility_col}")
-    print(f"row {best_row_utility}-{utility_row}={best_row_utility-utility_row}")
+    # utility of best response player against current row => best response player recieves -1 times the value
+    best_col_utility = -week1.best_response_value_row(matrix, row_strategy)
+
+
+    best_row_utility = -week1.best_response_value_column(matrix, column_strategy)
     return np.array([best_row_utility - utility_row, best_col_utility - utility_col])
 
 def nash_conv(matrix: np.array, row_strategy: np.array, column_strategy: np.array) -> float:
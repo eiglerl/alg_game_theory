7a6ceeb2accedc8c235e77c9fe3937f244d1489f
eiglerl <eiglerlukas@gmail.com>
2024-01-30 09:50:03 +0100

    new tests

--- tests/test_extended_form.py
+++ tests/test_extended_form.py
@@ -2,6 +2,48 @@ import numpy as np
 from libs import extensive_form_games
 import pytest
 
+def assert_dicts_equal(dict1: dict, dict2: dict):
+    for key in dict1.keys():
+        if key not in dict2:
+            raise AssertionError(f"Key '{key}' not found in the second dictionary")
+
+        value1, value2 = dict1[key], dict2[key]
+
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            # Recursively check nested dictionaries
+            assert_dicts_equal(value1, value2)
+        else:
+            assert dict1[key] == dict2[key] and not isinstance(value1, dict) and not isinstance(value2, dict)
+
+def avg_example_game() -> extensive_form_games.ExtensiveFormGame:
+    root = extensive_form_games.Node("Player1", "", 
+    {
+        "L": extensive_form_games.Node("Player2", "L", 
+        {
+            "L": extensive_form_games.Node("", "LL"),
+            "R": extensive_form_games.Node("", "LR")
+        }),
+        "R": extensive_form_games.Node("Player2", "R", 
+        {
+            "L": extensive_form_games.Node("", "RL"),
+            "R": extensive_form_games.Node("Player1", "RR", 
+            {
+                "L": extensive_form_games.Node("", "RRL"),
+                "R": extensive_form_games.Node("", "RRR")
+            })
+        })
+    })
+    matrix = {
+        "LL": {"Player1": -2, "Player2": 2},
+        "LR": {"Player1": 1, "Player2": -1},
+        "RL": {"Player1": 1, "Player2": -1},
+        "RRL": {"Player1": 4, "Player2": -4},
+        "RRR": {"Player1": 0, "Player2": 0},
+    }
+    tree = extensive_form_games.ExtensiveFormGame(["Player1", "Player2"], [], root, matrix)
+    return tree
+
+
 def test_reach_probability_rps_uniform():
     game = extensive_form_games.rps()
     actions = ["R", "P", "S"]
@@ -88,7 +130,7 @@ def test_reach_probability_simple_poker():
     probabilities = [game.calculate_reach_probability(strategies, h) for h in histories]
     assert np.allclose(probabilities, expected, atol=1e-8)
 
-def test_deltas_rps():
+def test_deltas_rps_simple():
     game = extensive_form_games.rps()
     p1_strat = {
         "Player1": {
@@ -112,7 +154,7 @@ def test_deltas_rps():
     strategies = p1_strat
     strategies.update(p2_strat)
     
-    # u_i(br(pi_-i), pi_-i) - u_i(pi)
+    # delta_i = u_i(br(pi_-i), pi_-i) - u_i(pi)
     exptected_deltas = {
         "Player1": 1 - (-1),
         "Player2": 1 - 1
@@ -120,12 +162,113 @@ def test_deltas_rps():
     
     deltas = game.calculate_deltas(strategies)
     
-    # Convert dictionaries to arrays
-    array1 = np.array(list(exptected_deltas.items()))
-    array2 = np.array(list(deltas.items()))
+    assert_dicts_equal(exptected_deltas, deltas)
 
-    # Sort arrays to ensure the order of items doesn't affect the comparison
-    array1.sort(axis=0)
-    array2.sort(axis=0)
+def test_average_strat():
+    game = avg_example_game()
     
-    assert np.array_equal(array1, array2)
+    s1 = {
+        "Player1": {
+            "": {
+                "L": 0.2,
+                "R": 0.8
+            },
+            "RR": {
+                "L": 0.8,
+                "R": 0.2
+            }
+        }
+    }
+    s2 = {
+        "Player1": {
+            "": {
+                "L": 0.8,
+                "R": 0.2
+            },
+            "RR": {
+                "L": 0.2,
+                "R": 0.8
+            }
+        }
+    }
+    
+    avg = game.average_strategy([s1, s2])
+    expected = {
+        "Player1": {
+            "": {
+                "L": 0.5,
+                "R": 0.5
+            },
+            "RR": {
+                "L": 0.68,
+                "R": 0.32
+            }
+        }
+    }
+    
+    assert_dicts_equal(expected, avg)
+    
+
+
+def test_best_response_rps():
+    game = extensive_form_games.rps()
+    strategies = {
+        "Player1": {
+            "": {
+                "R": 1,
+                "P": 0,
+                "S": 0
+            }
+        }
+    }
+    expected = {
+        "Player2": {
+            "": {
+                "R": 0,
+                "P": 1,
+                "S": 0
+            }
+        }
+    }
+    
+    br = game.best_response(strategies)
+    
+    assert_dicts_equal(expected, br)
+    
+def test_best_response_simple_poker():
+    game = extensive_form_games.kuhn_poker()
+    strategies = {
+        "Player1": {
+            "A": {
+                "C": 0.9,
+                "B": 0.1
+            },
+            "ACB": {
+                "C": 0.9,
+                "F": 0.1
+            },
+            "K": {
+                "C": 0.1,
+                "B": 0.9
+            },
+            "KCB": {
+                "C": 0.1,
+                "F": 0.9
+            }
+        },
+        "Chance": {
+            "": {
+                "A": 0.5,
+                "K": 0.5
+            }
+        }
+    }
+    
+    br = game.best_response(strategies)
+    print(br)
+    
+    strategies.update(br)
+    
+    deltas = game.calculate_deltas(strategies)
+    print(br)
+    
